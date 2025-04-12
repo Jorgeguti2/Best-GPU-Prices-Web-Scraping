05f233891ee7f8628730b7de3f774ec0b44d6561
Jorge Gutierrez <79301427+Jorgeguti2@users.noreply.github.com>
2025-04-11 23:41:00 -0700

    Saving progress

---
+++ best_gpu_price.py
@@ -0,0 +1,16 @@
+from bs4 import BeautifulSoup
+import requests
+import re
+
+gpu_Name = input("What GPU are you searching for?\n")
+
+newegg_Url = f"https://www.newegg.com/p/pl?d={gpu_Name}&N=4131"
+
+newegg_Page = requests.get(newegg_Url).text
+
+doc = BeautifulSoup(newegg_Page, "html.parser")
+
+page_Text = doc.find(class_="list-tool-pagination-text").strong.text
+print(page_Text)
+max_Pages = re.compile("[^/]+$")
+print(max_Pages)
